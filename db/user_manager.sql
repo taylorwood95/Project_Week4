606da4b59b0028f899f61527a3c6e62a7eb3652d
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-02-03 12:08:33 +0000

    database set up

---
+++ db/user_manager.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    capital VARCHAR (255),
+    currency VARCHAR (255),
+    review text
+);
+
---
+++ models/countries.py
@@ -0,0 +1,9 @@
+class Country:
+
+    def __inti__(self, name, capital, currency, review, id = None):
+        self.name = name
+        self.capital = capital
+        self.currency = currency
+        self.review = review
+        self.id = id
+        
