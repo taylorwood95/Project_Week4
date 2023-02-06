7fea16afcedc977ce37b6177869cd74deb86a066
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-02-06 20:39:22 +0000

    bug still with update button

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -7,33 +7,38 @@ import repositories.user_repository as user_repository
 
 countries_blueprint = Blueprint("countries", __name__)
 
-# GET countries 
+# GET countries
 @countries_blueprint.route("/countries")
 def show_countries():
     users = user_repository.select_all()
     countries = country_repository.select_all()
-    return render_template("/countries/new_country.html", all_countries=countries, all_users = users)
+    return render_template(
+        "/countries/new_country.html", all_countries=countries, all_users=users
+    )
 
-# ADD countries 
+
+# ADD countries
 @countries_blueprint.route("/countries", methods=["POST"])
 def create_country():
-    user_id  = request.form['user_id']
-    name = request.form['name']
-    capital = request.form['capital']
-    currency = request.form['currency']
-    review = request.form['review']
-   
+    user_id = request.form["user_id"]
+    name = request.form["name"]
+    capital = request.form["capital"]
+    currency = request.form["currency"]
+    review = request.form["review"]
+
     user = user_repository.select(user_id)
     country = Country(name, capital, currency, review, user)
     country_repository.save(country)
     return redirect("/countries")
 
+
 @countries_blueprint.route("/countries/<int:id>", methods=["GET"])
 def show_country(id):
-    
+
     country = country_repository.select(id)
-   
-    return render_template("/countries/show.html",country=country)
+
+    return render_template("/countries/show.html", country=country)
+
 
 @countries_blueprint.route("/countries/<int:id>/edit", methods=["GET"])
 def edit_country(id):
@@ -41,27 +46,21 @@ def edit_country(id):
     user = user_repository.select_all()
     return render_template("countries/edit.html", country=country, user=user)
 
-@countries_blueprint.route("/countries/<int:id>", methods=['POST'])
+
+@countries_blueprint.route("/countries/<int:id>", methods=["POST"])
 def update_country(id):
-    user_id  = request.form['user_id']
-    name = request.form['name']
-    capital = request.form['capital']
-    currency = request.form['currency']
-    review = request.form['review']
+    user_id = request.form["user_id"]
+    name = request.form["name"]
+    capital = request.form["capital"]
+    currency = request.form["currency"]
+    review = request.form["review"]
     user = user_repository.select(user_id)
-    country = Country(name, capital, currency, review, user)
+    country = Country(name, capital, currency, review, user, id)
     country_repository.update(country)
     return redirect("/countries")
+
 
 @countries_blueprint.route("/countries/<id>/delete", methods=["POST"])
 def delete_country(id):
     country_repository.delete(id)
     return redirect("/countries")
-
-
-
-
-
-
-
-
